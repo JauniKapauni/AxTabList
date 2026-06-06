c879bb84ca832dc9de6e038c7d907d2c3ff83ad7
JauniKapauni <riches-goods.54@icloud.com>
2026-06-06 02:00:17 +0200

    feat: add basic tablist functionality

--- src/main/java/de/jaunikapauni/axtablist/AxTabList.java
+++ src/main/java/de/jaunikapauni/axtablist/AxTabList.java
@@ -1,9 +1,18 @@
 package de.jaunikapauni.axtablist;
 
 import com.google.inject.Inject;
+import com.velocitypowered.api.event.player.ServerPostConnectEvent;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.plugin.Plugin;
+import net.kyori.adventure.text.Component;
 import org.slf4j.Logger;
+
+@Plugin(
+        id = "axtablist",
+        name = "AxTabList",
+        version = "0.0.0"
+)
 
 public class AxTabList {
 
@@ -12,5 +21,10 @@ public class AxTabList {
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
       // Plugin initialization logic goes here
+    }
+
+    @Subscribe
+    public void onJoin(ServerPostConnectEvent e){
+        e.getPlayer().sendPlayerListHeaderAndFooter(Component.text("header"), Component.text("footer"));
     }
 }
